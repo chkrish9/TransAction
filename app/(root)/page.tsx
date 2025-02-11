009a9fcb0018d8102d1a2797ddf485ce7de0ef2b
murali <murali.24.4.91@gmail.com>
2025-02-10 19:40:30 -0600

    Adding total balance block

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import Header from "@/components/header";
+import TotalBalance from "@/components/totalBalance";
 import React from "react";
 
+const loggedIn = { firstName: "Murali" };
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Header
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficently."
+          />
+        </header>
+        <TotalBalance
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
